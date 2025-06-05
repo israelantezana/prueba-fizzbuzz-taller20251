e47cbb532c47747b9c8a4e2ac97b39c919d56b80
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-05 11:12:54 -0400

    ciclo de pruebas pasando

--- src/fizzbuz.spec.js
+++ src/fizzbuz.spec.js
@@ -8,4 +8,8 @@ describe("Fizzbuzz", () => {
   it("retornar el numero si no sigue una regla con otro ejemplo", () => {
     expect(fizzbuzz(2)).toEqual(2);
   });
+
+  it("retornar el numero si no sigue una regla con otro ejemplo", () => {
+    expect(fizzbuzz(4)).toEqual(4);
+  });
 });
