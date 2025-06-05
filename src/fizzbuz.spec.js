425da388b2dc7d0037bbd8a16c08399da6e57dc7
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-05 11:08:05 -0400

    2do ciclo tdd, retorna el numero que no tiene reglas

--- src/fizzbuz.spec.js
+++ src/fizzbuz.spec.js
@@ -2,10 +2,14 @@
 
 describe("Fizzbuzz", () => {
   it("retornar el numero si no sigue una regla", () => {
-    expect(fizzbuzz("1")).toEqual(1);
+    expect(fizzbuzz(1)).toEqual(1);
+  });
+
+  it("retornar el numero si no sigue una regla con otro ejemplo", () => {
+    expect(fizzbuzz(2)).toEqual(2);
   });
 });
 
 function fizzbuzz(n) {
-  return 1;
+  return n;
 }
