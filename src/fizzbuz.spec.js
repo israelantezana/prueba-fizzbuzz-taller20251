94944820c99577a903a22ef329a66554e738dde2
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-05 11:02:05 -0400

    Primer ciclo tdd, retorna el mismo numero

---
+++ src/fizzbuz.spec.js
@@ -0,0 +1,11 @@
+//import sumar from "./sumador.js";
+
+describe("Fizzbuzz", () => {
+  it("retornar el numero si no sigue una regla", () => {
+    expect(fizzbuzz("1")).toEqual(1);
+  });
+});
+
+function fizzbuzz(n) {
+  return 1;
+}
