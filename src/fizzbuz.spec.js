964c5e81c1f3066e46dcae9069f1ec7b2d8b87c0
Israel Antezana Rojas <israelantezana@gmail.com>
2025-06-05 11:21:51 -0400

    Baja la cobertura?

--- src/fizzbuz.spec.js
+++ src/fizzbuz.spec.js
@@ -12,4 +12,8 @@ describe("Fizzbuzz", () => {
   it("retornar el numero si no sigue una regla con otro ejemplo", () => {
     expect(fizzBuzz(4)).toEqual(4);
   });
+
+  it("retornar otro ejemplo pero con baja cobertura y muchas lineas", () => {
+    expect(fizzBuzz(7)).toEqual(7);
+  });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,12 @@
 function fizzBuzz(n) {
+  const a = 4 + 5;
+  const b = a * 2;
+  let c = (b * a) / 2;
+  const d = 4 + 5;
+  const e = a * 2;
+  let f = (b * a) / 2;
+  if (a > 2) c = 2;
+
   return n;
 }
 
